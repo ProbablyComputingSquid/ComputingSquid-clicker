3521537d9b946f16ffb3d192fa285b1b23ae7586
squids are cool <jwbolling001@gmail.com>
2020-08-19 15:41:37 +0000

    A lot added some shop

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 //vars
-var csLeftToWin = 1000000
+function start() {
+  window.location.href='play.html'
+}
+
+
+var csLeftToWin = 10000;
 var squids = 0;
 var squidsPerClick = 1;
 var computers = 0;
@@ -9,43 +14,87 @@ var moneyPerClick = 1;
 var computingSquids = 0;
 var computingSquidsPerClick = 0;
 
-//get things per click
-function addSquids() {
-  squids += squidsPerClick;
-  document.getElementById("squids").innerHTML = "Your Squids: " + squids;
+
+
+function restart() {
+  
+  var csLeftToWin = 10000;
+  var squids = 0;
+  var squidsPerClick = 1;
+  var computers = 0;
+  var computersPerClick = 1;
+  var money = 0;
+  var moneyPerClick = 1;
+  var computingSquids = 0;
+  var computingSquidsPerClick = 0;
+  start();
+
 }
 
-function addComputers() {
-  computers += computersPerClick;
-  document.getElementById("computers").innerHTML = "Your Computers: " + computers;
-}
-
-function getMoney() {
-  money += moneyPerClick;
-  document.getElementById("money").innerHTML = "Your Squid Bills: " + money;
-}
-
-//if win
-function checkIfWin() {
-  if csLeftToWin <= {
-  window.location.assign = ("https://computingsquid-clicker.computingsquid.repl.co/win.html");
-  } else {
-    document.getElementById("CSsToWin").innerHTML = ;
-  }
-}
-
-//Transactioin
-
-
-function createCS() {
-  if (money >= 1 && computers >= 10 && squids >= 10) {
-  money -= 5;
-  computers -= 5;
-  squids -= 5;
-  computingSquids += 2;
+function update() {
   document.getElementById("squids").innerHTML = "Your Squids: " + squids;
   document.getElementById("computers").innerHTML = "Your Computers: " + computers;
   document.getElementById("money").innerHTML = "Your Squid Bills: " + money;
   document.getElementById("CSs").innerHTML = "Your Computing Squids: " + computingSquids;
+  document.getElementById("CSsToWin").innerHTML = csLeftToWin - computingSquids;
+  csLeftToWin -= computingSquids;
+  checkIfWin();
+  document.getElementById("cpc").innerHTML = computersPerClick;
+  document.getElementById("spc").innerHTML = squidsPerClick;
+  document.getElementById("sbpc").innerHTML = squidBillsPerClick;
+  
+}
+//get things per click
+function addSquids() {
+  squids += squidsPerClick;
+  update();
+}
+
+function addComputers() {
+  computers += computersPerClick;
+  update();
+}
+
+function getMoney() {
+  money += moneyPerClick;
+  update();
+}
+
+//if win
+function checkIfWin() {
+  if (csLeftToWin <= 0) {
+    window.location.href='win.html';
+  } 
+}
+
+//Transactioin
+function createCS() {
+  if (money >= 3 && computers >= 5 && squids >= 5) {
+    money -= 3;
+    computers -= 5;
+    squids -= 5;
+    computingSquids += 4;
+    update();
+  }
+}
+
+//shop
+function buySquid() {
+  if (squids >= 20) {
+    squids -= 20
+    squidsPerClick += 1;
+    update();
+  } else {
+    alert("Sorry, you don't have enough squids for More Tentacles, you need 20 squids.");
+  }
+}
+
+function buyComputer() {
+  if (computers >= 20) {
+    computers -= 20
+    computersPerClick += 1;
+    update();
+  } else {
+    alert("Sorry, you don't have enough computers for a Software Update, you need 20 computers.");
   }
 }
