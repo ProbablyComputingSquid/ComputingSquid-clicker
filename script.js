e9bff0c144d411c004e12aa5d7e1803388ea08f2
Emoji Game <jchang3@madison.k12.wi.us>
2020-08-19 20:00:17 +0000

    Broke some stuff :)

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
-//vars
+//start
 function start() {
-  window.location.href='play.html'
-}
-
-
-var csLeftToWin = 10000;
-var squids = 0;
-var squidsPerClick = 1;
-var computers = 0;
-var computersPerClick = 1;
-var money = 0;
-var moneyPerClick = 1;
-var computingSquids = 0;
-var computingSquidsPerClick = 0;
-
-
-
-function restart() {
-  
   var csLeftToWin = 10000;
   var squids = 0;
   var squidsPerClick = 1;
@@ -25,10 +7,19 @@ function restart() {
   var computersPerClick = 1;
   var money = 0;
   var moneyPerClick = 1;
-  var computingSquids = 0;
-  var computingSquidsPerClick = 0;
-  start();
+  var squidsPerSecond = 0;
+  var computersPerSecond = 0;
+  var moneyPerSecond = 0;
+  var buy1cost = 20;
+  var buy2cost= 20;
+  var buy3cost = 35;
+  var buy4cost = 50;
+  window.location.href='play.html'
 
+}
+
+function restart() {
+  start();
 }
 
 function update() {
@@ -41,9 +32,17 @@ function update() {
   checkIfWin();
   document.getElementById("cpc").innerHTML = computersPerClick;
   document.getElementById("spc").innerHTML = squidsPerClick;
-  document.getElementById("sbpc").innerHTML = squidBillsPerClick;
-  
+  document.getElementById("sbpc").innerHTML = moneyPerClick;
+  buy1cost = (spc ** 2) / 5 + 20;
+  buy2cost = (spc ** 2) / 5 + 20;
+  buy3cost = (spc ** 2) / 5 + 35;
+  buy4cost = (spc ** 2) / 3 + 50;
+  document.getElementById("buy1cost").innerHTML = buy1cost;
+  document.getElementById("buy2cost").innerHTML = buy2cost;
+  document.getElementById("buy3cost").innerHTML = buy3cost;
+  document.getElementById("buy4cost").innerHTML = buy4cost;
 }
+
 //get things per click
 function addSquids() {
   squids += squidsPerClick;
@@ -79,22 +78,46 @@ function createCS() {
 }
 
 //shop
-function buySquid() {
-  if (squids >= 20) {
-    squids -= 20
+function buy1() {
+  if (squids >= buy1cost) {
+    squids -= buy1cost;
     squidsPerClick += 1;
+    buy1cost = Math.floor((x ** 2) / 5 + 20);
     update();
   } else {
-    alert("Sorry, you don't have enough squids for More Tentacles, you need 20 squids.");
+    alert("Sorry, you don't have enough squids for More Tentacles, you need at least " + buy1cost + " squids.");
   }
 }
 
-function buyComputer() {
-  if (computers >= 20) {
-    computers -= 20
+function buy2() {
+  if (computers >= buy2cost) {
+    computers -= buy2cost;
     computersPerClick += 1;
+    buy2cost = Math.floor((x ** 2) / 5 + 20);
     update();
   } else {
-    alert("Sorry, you don't have enough computers for a Software Update, you need 20 computers.");
+    alert("Sorry, you don't have enough computers for a Software Update, you need at least " + buy2cost + " computers.");
+  }
+}
+
+function buy3() {
+  if (money >= buy3cost) {
+    money -= buy3cost;
+    moneyPerClick += 1;
+    buy3cost = Math.floor((x ** 2) / 5 + 35)
+    update();
+  } else {
+    alert("Sorry, you don't have enough squid bills for a Salary Increase, you need at least " + buy3cost + " squid bills.");
+  }
+}
+
+function buy4() {
+  if (computingSquids >= buy4cost) {
+    computingSquids -= buy4cost;
+    squidsPerSecond += 1;
+    buy4cost = Math.floor((x ** 2) / 3 + 50)
+    update();
+  } else {
+    alert("Sorry, you don't have enough ComputingSquids for a Tiny Squid, you need at least " + buy4cost + " ComputingSquids.");
   }
 }
