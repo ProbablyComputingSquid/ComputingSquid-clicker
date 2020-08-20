ed4f77f3624f5206966e3f9cdad4f8110f4478eb
Emoji Game <jchang3@madison.k12.wi.us>
2020-08-20 18:30:45 +0000

    Shop is now in one function! (Well, two, but whatever) Also made it look nicer

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-//start
+//vars
 function start() {
+  window.location.href='play.html'
+}
+
+function restart() {
   var csLeftToWin = 10000;
   var squids = 0;
   var squidsPerClick = 1;
@@ -7,18 +11,8 @@ function start() {
   var computersPerClick = 1;
   var money = 0;
   var moneyPerClick = 1;
-  var squidsPerSecond = 0;
-  var computersPerSecond = 0;
-  var moneyPerSecond = 0;
-  var buy1cost = 20;
-  var buy2cost= 20;
-  var buy3cost = 35;
-  var buy4cost = 50;
-  window.location.href='play.html'
-
-}
-
-function restart() {
+  var computingSquids = 0;
+  var computingSquidsPerClick = 0;
   start();
 }
 
@@ -32,18 +26,10 @@ function update() {
   checkIfWin();
   document.getElementById("cpc").innerHTML = computersPerClick;
   document.getElementById("spc").innerHTML = squidsPerClick;
-  document.getElementById("sbpc").innerHTML = moneyPerClick;
-  buy1cost = (spc ** 2) / 5 + 20;
-  buy2cost = (spc ** 2) / 5 + 20;
-  buy3cost = (spc ** 2) / 5 + 35;
-  buy4cost = (spc ** 2) / 3 + 50;
-  document.getElementById("buy1cost").innerHTML = buy1cost;
-  document.getElementById("buy2cost").innerHTML = buy2cost;
-  document.getElementById("buy3cost").innerHTML = buy3cost;
-  document.getElementById("buy4cost").innerHTML = buy4cost;
+  document.getElementById("sbpc").innerHTML = squidBillsPerClick;
+  
 }
-
-//get things per click
+//Get things per click
 function addSquids() {
   squids += squidsPerClick;
   update();
@@ -59,14 +45,7 @@ function getMoney() {
   update();
 }
 
-//if win
-function checkIfWin() {
-  if (csLeftToWin <= 0) {
-    window.location.href='win.html';
-  } 
-}
-
-//Transactioin
+//Transaction
 function createCS() {
   if (money >= 3 && computers >= 5 && squids >= 5) {
     money -= 3;
@@ -77,47 +56,38 @@ function createCS() {
   }
 }
 
-//shop
-function buy1() {
-  if (squids >= buy1cost) {
-    squids -= buy1cost;
-    squidsPerClick += 1;
-    buy1cost = Math.floor((x ** 2) / 5 + 20);
-    update();
-  } else {
-    alert("Sorry, you don't have enough squids for More Tentacles, you need at least " + buy1cost + " squids.");
-  }
+//If win
+function checkIfWin() {
+  if (csLeftToWin <= 0) {
+    window.location.href='win.html';
+  } 
 }
 
-function buy2() {
-  if (computers >= buy2cost) {
-    computers -= buy2cost;
-    computersPerClick += 1;
-    buy2cost = Math.floor((x ** 2) / 5 + 20);
-    update();
-  } else {
-    alert("Sorry, you don't have enough computers for a Software Update, you need at least " + buy2cost + " computers.");
+//Shop
+function buyScript(id2, id3, thing, thingPerClick, cost, errMssg) {
+  var newThing = thing;
+  var newThingPerClick = thingPerClick;
+  if (id2 == id3) {
+    if (thing >= cost) {
+      newThing -= cost;
+      newThingPerClick += 1;
+    } else {
+      alert(errMssg);
+    }
   }
+  return [newThing, newThingPerClick]
 }
 
-function buy3() {
-  if (money >= buy3cost) {
-    money -= buy3cost;
-    moneyPerClick += 1;
-    buy3cost = Math.floor((x ** 2) / 5 + 35)
-    update();
-  } else {
-    alert("Sorry, you don't have enough squid bills for a Salary Increase, you need at least " + buy3cost + " squid bills.");
-  }
-}
+function buy(id) {
+  var buy1 = buyScript(id, 1, squids, squidsPerClick, 20, "Sorry! You don't have enough squids.");
+  var buy2 = buyScript(id, 2, computers, computersPerClick, 20, "Sorry! You don't have enough money.");
+  var buy3 = buyScript(id, 3, money, moneyPerClick, 35, "Sorry! You don't have enough squid bills.");
 
-function buy4() {
-  if (computingSquids >= buy4cost) {
-    computingSquids -= buy4cost;
-    squidsPerSecond += 1;
-    buy4cost = Math.floor((x ** 2) / 3 + 50)
-    update();
-  } else {
-    alert("Sorry, you don't have enough ComputingSquids for a Tiny Squid, you need at least " + buy4cost + " ComputingSquids.");
-  }
+  squids = buy1[0];
+  squidsPerClick = buy1[1];
+  computers = buy2[0];
+  computersPerClick = buy2[1];
+  money = buy3[0];
+  moneyPerClick = buy3[1];
+  update();
 }
