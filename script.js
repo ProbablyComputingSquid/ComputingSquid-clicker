1f2edaa0026ec5e9520f94fe08acaeee00abb3df
squids are cool <jwbolling001@gmail.com>
2020-08-25 19:42:27 +0000

    second

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 function start() {
   window.location.href='play.html'
 }
-
+alert("Please Wait, The page will take time to load...");
 var csLeftToWin = 1000;
 var squids = 0;
-var squidsPerClick = 1;
+var spc = 1;
 var computers = 0;
-var computersPerClick = 1;
+var cpc = 1;
 var money = 0;
-var moneyPerClick = 1;
+var sbpc = 1;
 var computingSquids = 0;
-var computingSquidsPerClick = 0;
+var computingspc = 0;
 var cps = 0;
 var sps = 0;
 var sbps = 0;
@@ -19,13 +19,12 @@ var sbps = 0;
 function restart() {
   var csLeftToWin = 1000;
   var squids = 0;
-  var squidsPerClick = 1;
+  var spc = 1;
   var computers = 0;
-  var computersPerClick = 1;
+  var cpc = 1;
   var money = 0;
-  var moneyPerClick = 1;
+  var sbpc = 1;
   var computingSquids = 0;
-  var computingSquidsPerClick = 0;
   var cps = 0;
   var sps = 0;
   var sbps = 0;
@@ -37,38 +36,37 @@ function update() {
   document.getElementById("computers").innerHTML = "Your Computers: " + computers;
   document.getElementById("money").innerHTML = "Your Squid Bills: " + money;
   document.getElementById("CSs").innerHTML = "Your Computing Squids: " + computingSquids;
-  document.getElementById("cpc").innerHTML = computersPerClick;
-  document.getElementById("spc").innerHTML = squidsPerClick;
-  document.getElementById("sbpc").innerHTML = squidBillsPerClick;
-  
+  document.getElementById("cpc").innerHTML = cpc;
+  document.getElementById("spc").innerHTML = spc;
+  document.getElementById("sbpc").innerHTML = sbpc;
+  csLeftToWin = 1000 - computingSquids;
+  document.getElementById("CSsToWin").innerHTML = csLeftToWin;
 }
 
 function updateS2() {
   squids += sps;
   computers += cps;
   money += sbps;
+  document.getElementById("cps").innerHTML = cps;
+  document.getElementById("sps").innerHTML = sps;
+  document.getElementById("sbps").innerHTML = sbps;
   update();
 }
-function updateS() {
- setTimeout(updateS2, 1000);
- document.getElementById("cps").innerHTML = cps;
- document.getElementById("sps").innerHTML = sps;
- document.getElementById("sbps").innerHTML = sbps;
-}
-
+//update seconds?
+setInterval(updateS2, 1000)
 //get things per click
 function addSquids() {
-  squids += squidsPerClick;
+  squids += spc;
   update();
 }
 
 function addComputers() {
-  computers += computersPerClick;
+  computers += cpc;
   update();
 }
 
 function getMoney() {
-  money += moneyPerClick;
+  money += sbpc;
   update();
 }
 
@@ -86,8 +84,6 @@ function createCS() {
     computers -= 5;
     squids -= 5;
     computingSquids += 4;
-    document.getElementById("CSsToWin").innerHTML = csLeftToWin - computingSquids;
-   csLeftToWin -= computingSquids;
    checkIfWin();
     update();
   }
@@ -117,34 +113,38 @@ function buyScript(id2, id3, thing, thingPerClick, cost, errMssg) {
 
 function buy(id) {
   //Buys stuff 
-  var buy1 = buyScript(id, 1, squids, squidsPerClick, 20, "Sorry! You don't have enough squids.");
-  var buy2 = buyScript(id, 2, computers, computersPerClick, 20, "Sorry! You don't have enough money.");
-  var buy3 = buyScript(id, 3, money, moneyPerClick, 35, "Sorry! You don't have enough squid bills.");
+  var buy1 = buyScript(id, 1, squids, spc, 20, "Sorry! You don't have enough squids.");
+  var buy2 = buyScript(id, 2, computers, cpc, 20, "Sorry! You don't have enough money.");
+  var buy3 = buyScript(id, 3, money, sbpc, 35, "Sorry! You don't have enough squid bills.");
 
   //Resets the variables
   squids = buy1[0];
-  squidsPerClick = buy1[1];
+  spc = buy1[1];
   computers = buy2[0];
-  computersPerClick = buy2[1];
+  cpc = buy2[1];
   money = buy3[0];
-  moneyPerClick = buy3[1];
+  sbpc = buy3[1];
   update();
 }
 
 function buy2(num) {
   //buys things
   switch(num) {
-    case 1:
+    case 1://tiny Squid
      if(computingSquids >= 16) {
        computingSquids -= 16;
-       squidsPerSecond += 1;
+       sps += 1;
        update();
      } else {
        alert("You need at least 16 ComputingSquids To get tiny squid.");
      }
      break;
-    case 2:
-     //to do
+    case 2://autoclicker
+     if(computingSquids >= 16){
+       computingSquids -= 16;
+       cps += 1;
+       update();
+     }
      break;
     default:
      alert("Unrecognized selection, please try again.");
