5881b308cabe679c710482ced53557447e043b72
squids are cool <jwbolling001@gmail.com>
2020-08-25 14:45:54 +0000

    start day log... day one

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function start() {
   window.location.href='play.html'
 }
 
-var csLeftToWin = 10000;
+var csLeftToWin = 1000;
 var squids = 0;
 var squidsPerClick = 1;
 var computers = 0;
@@ -12,11 +12,12 @@ var money = 0;
 var moneyPerClick = 1;
 var computingSquids = 0;
 var computingSquidsPerClick = 0;
-
-
+var cps = 0;
+var sps = 0;
+var sbps = 0;
 
 function restart() {
-  var csLeftToWin = 10000;
+  var csLeftToWin = 1000;
   var squids = 0;
   var squidsPerClick = 1;
   var computers = 0;
@@ -25,8 +26,10 @@ function restart() {
   var moneyPerClick = 1;
   var computingSquids = 0;
   var computingSquidsPerClick = 0;
+  var cps = 0;
+  var sps = 0;
+  var sbps = 0;
   start();
-
 }
 
 function update() {
@@ -34,12 +37,23 @@ function update() {
   document.getElementById("computers").innerHTML = "Your Computers: " + computers;
   document.getElementById("money").innerHTML = "Your Squid Bills: " + money;
   document.getElementById("CSs").innerHTML = "Your Computing Squids: " + computingSquids;
-  document.getElementById("CSsToWin").innerHTML = csLeftToWin - computingSquids;
-  csLeftToWin -= computingSquids;
-  checkIfWin();
   document.getElementById("cpc").innerHTML = computersPerClick;
   document.getElementById("spc").innerHTML = squidsPerClick;
   document.getElementById("sbpc").innerHTML = squidBillsPerClick;
+  
+}
+
+function updateS2() {
+  squids += sps;
+  computers += cps;
+  money += sbps;
+  update();
+}
+function updateS() {
+ setTimeout(updateS2, 1000);
+ document.getElementById("cps").innerHTML = cps;
+ document.getElementById("sps").innerHTML = sps;
+ document.getElementById("sbps").innerHTML = sbps;
 }
 
 //get things per click
@@ -72,6 +86,9 @@ function createCS() {
     computers -= 5;
     squids -= 5;
     computingSquids += 4;
+    document.getElementById("CSsToWin").innerHTML = csLeftToWin - computingSquids;
+   csLeftToWin -= computingSquids;
+   checkIfWin();
     update();
   }
 }
@@ -99,10 +116,12 @@ function buyScript(id2, id3, thing, thingPerClick, cost, errMssg) {
 }
 
 function buy(id) {
+  //Buys stuff 
   var buy1 = buyScript(id, 1, squids, squidsPerClick, 20, "Sorry! You don't have enough squids.");
   var buy2 = buyScript(id, 2, computers, computersPerClick, 20, "Sorry! You don't have enough money.");
   var buy3 = buyScript(id, 3, money, moneyPerClick, 35, "Sorry! You don't have enough squid bills.");
 
+  //Resets the variables
   squids = buy1[0];
   squidsPerClick = buy1[1];
   computers = buy2[0];
@@ -110,4 +129,24 @@ function buy(id) {
   money = buy3[0];
   moneyPerClick = buy3[1];
   update();
+}
+
+function buy2(num) {
+  //buys things
+  switch(num) {
+    case 1:
+     if(computingSquids >= 16) {
+       computingSquids -= 16;
+       squidsPerSecond += 1;
+       update();
+     } else {
+       alert("You need at least 16 ComputingSquids To get tiny squid.");
+     }
+     break;
+    case 2:
+     //to do
+     break;
+    default:
+     alert("Unrecognized selection, please try again.");
+  }
 }
---
+++ textfiles/ideas.md
@@ -0,0 +1,9 @@
+# Ideas
+#### Squids
+___
++ Squid Squad upgrade
++ Feeding Ground upgrade
+___
+#### Computers
+___
++ Firefox upgrade
