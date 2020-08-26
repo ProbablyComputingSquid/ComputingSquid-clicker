d70c924d25ab2edc01f9b7af4bb2af0152b31bd7
Emoji Game <jchang3@madison.k12.wi.us>
2020-08-26 16:45:18 +0000

    stuff lol

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 //vars
-function start() {
-  window.location.href='play.html'
-}
-alert("Please Wait, The page will take time to load...");
 var csLeftToWin = 1000;
 var squids = 0;
 var spc = 1;
@@ -11,10 +7,13 @@ var cpc = 1;
 var money = 0;
 var sbpc = 1;
 var computingSquids = 0;
-var computingspc = 0;
 var cps = 0;
 var sps = 0;
 var sbps = 0;
+  
+function start() {
+  window.location.href='play.html';
+}
 
 function restart() {
   var csLeftToWin = 1000;
@@ -30,10 +29,30 @@ function restart() {
   var sbps = 0;
   start();
 }
-
+//local storage
+if(typeof(Storage) !== "undefined") {
+  var saveData = {"squids" : squids, "computers" : computers, "money" : money, "computingSquids" : computingSquids, "spc" : spc, "cpc" : cpc, "sbpc" : sbpc, "sps" : sps, "cps" : cps, "sbps" : sbps, "csLeftToWin" : csLeftToWin};
+  for (i = 0; i <= Object.keys(saveData).length, i++) {
+    localStorage.setItem( , 0);
+  }
+  localStorage.setItem("squids" , 0);
+  localStorage.setItem("spc" , "1");
+  localStorage.setItem("sps" , "1")
+  localStorage.setItem("computers" , "0");
+  localStorage.setItem("cpc" , "1");
+  localStorage.setItem("cps" , "0");
+  localStorage.setItem("money" , "0");
+  localStorage.setItem("sbpc" , "1");
+  localStorage.setItem("sbps" , "0");
+  localStorage.setItem("computingSquids" , "0")
+  localStorage.setItem("csLeftToWin" , "1000")
+} else {
+  alert("Local storage is unavaliabe. Please enable it, update your browser, or use a different browser like firefox. If you're already using firefox but older, then update it. LOL");
+  alert("do not try to play the game. it will mess it up.");
+}
 function update() {
-  document.getElementById("squids").innerHTML = "Your Squids: " + squids;
-  document.getElementById("computers").innerHTML = "Your Computers: " + computers;
+  document.getElementById("squids").innerHTML = "Your Squids: " + localStorage.squids;
+  document.getElementById("computers").innerHTML = "Your Computers: " + localStorage.computers;
   document.getElementById("money").innerHTML = "Your Squid Bills: " + money;
   document.getElementById("CSs").innerHTML = "Your Computing Squids: " + computingSquids;
   document.getElementById("cpc").innerHTML = cpc;
@@ -43,7 +62,7 @@ function update() {
   document.getElementById("CSsToWin").innerHTML = csLeftToWin;
 }
 
-function updateS2() {
+function updateV2() {
   squids += sps;
   computers += cps;
   money += sbps;
@@ -52,8 +71,7 @@ function updateS2() {
   document.getElementById("sbps").innerHTML = sbps;
   update();
 }
-//update seconds?
-setInterval(updateS2, 1000)
+
 //get things per click
 function addSquids() {
   squids += spc;
@@ -61,7 +79,7 @@ function addSquids() {
 }
 
 function addComputers() {
-  computers += cpc;
+  localStorage.computers += cpc;
   update();
 }
 
@@ -79,9 +97,9 @@ function checkIfWin() {
 
 //Transaction
 function createCS() {
-  if (money >= 3 && computers >= 5 && squids >= 5) {
+  if (money >= 3 && localStorage.computers >= 5 && squids >= 5) {
     money -= 3;
-    computers -= 5;
+    localStorage.computers -= 5;
     squids -= 5;
     computingSquids += 4;
    checkIfWin();
@@ -97,15 +115,15 @@ function checkIfWin() {
 }
 
 //Shop
-function buyScript(id2, id3, thing, thingPerClick, cost, errMssg) {
+function buyScript(id2, id3, thing, thingPerClick, cost, increase,errMssg) {
   var newThing = thing;
   var newThingPerClick = thingPerClick;
   if (id2 == id3) {
     if (thing >= cost) {
       newThing -= cost;
-      newThingPerClick += 1;
+      newThingPerClick += increase;
     } else {
-      alert(errMssg);
+     console.log(errMssg);
     }
   }
   return [newThing, newThingPerClick]
@@ -113,9 +131,10 @@ function buyScript(id2, id3, thing, thingPerClick, cost, errMssg) {
 
 function buy(id) {
   //Buys stuff 
-  var buy1 = buyScript(id, 1, squids, spc, 20, "Sorry! You don't have enough squids.");
-  var buy2 = buyScript(id, 2, computers, cpc, 20, "Sorry! You don't have enough money.");
-  var buy3 = buyScript(id, 3, money, sbpc, 35, "Sorry! You don't have enough squid bills.");
+  var buy1 = buyScript(id, 1, squids, spc, 20, 1,"Sorry! You don't have enough squids.");
+  var buy2 = buyScript(id, 2, computers, cpc, 20, 1,"Sorry! You don't have enough money.");
+  var buy3 = buyScript(id, 3, money, sbpc, 35, 1,"Sorry! You don't have enough squid bills.");
+  var buy4 = buyScript(id, 4, squids, sps, 60, 5,"Sorry You don't have enough squids.");
 
   //Resets the variables
   squids = buy1[0];
@@ -124,29 +143,24 @@ function buy(id) {
   cpc = buy2[1];
   money = buy3[0];
   sbpc = buy3[1];
+  squids = buy4[0];
+  spc = buy4[1];
   update();
 }
 
-function buy2(num) {
+function buy2(letter) {
   //buys things
-  switch(num) {
-    case 1://tiny Squid
-     if(computingSquids >= 16) {
-       computingSquids -= 16;
-       sps += 1;
-       update();
-     } else {
-       alert("You need at least 16 ComputingSquids To get tiny squid.");
-     }
-     break;
-    case 2://autoclicker
-     if(computingSquids >= 16){
-       computingSquids -= 16;
-       cps += 1;
-       update();
-     }
-     break;
-    default:
-     alert("Unrecognized selection, please try again.");
-  }
+  var buyA = buyScript(id, 1, computingSquids, sps, 24, 1,"Sorry! You don't have enough computingSquids.");
+  var buyB = buyScript(id, 2, computingSquids, cps, 24, 1,"Sorry! You don't have enough computingSquids.");
+  var buyC = buyScript(id, 3, computingSquids, sbps, 35, 1,"Sorry! You don't have enough computingSquids.");
+
+  computingSquids = buyA[0];
+  sps = buyA[1];
+  computingSquids = buyB[0]
+  cps = buyB[1];
+  computingSquids = buyC[0]
+  sbps = buyC[1];
 }
+
+//update seconds
+setInterval(updateV2, 1000);
